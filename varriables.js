74d85873d718da1dacf948c73f0706f4f5d48e94
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-09-20 16:59:28 +0000

    toFixed use

---
+++ varriables.js
@@ -0,0 +1,9 @@
+// PH 17-8 Different variable types and use toFixed with parseFloat
+
+var first =0.1;
+var second =0.2;
+
+var total = first + second;
+
+console.log(total);
+console.log(total.toFixed(1));
