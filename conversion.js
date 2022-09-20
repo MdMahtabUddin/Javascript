4397718f5f3e8692bfa2e2fd464034e50fca2768
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-09-20 16:45:01 +0000

    added

--- conversion.js
+++ conversion.js
@@ -1,2 +1,27 @@
 // PH anced) String Concatenation, Integer float parseInt parseFloat type conversion
 
+// Concatention 
+
+var firstName ='Mahtab';
+var lastName ='Uddin'
+
+var fullNmae = firstName + ' ' + lastName;
+console.log(fullNmae);
+
+var onionPrice ='55';
+var eggPrice ='45';
+
+
+var total = onionPrice + eggPrice;
+console.log(total);
+// integer number 
+var onion =parseInt(onionPrice);
+// floation number  ex 1.99
+var egg =parseFloat(eggPrice);
+console.log(egg);
+var totalPrice = onion +egg;
+console.log(totalPrice);
+
+
+
+
