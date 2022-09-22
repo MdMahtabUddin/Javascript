60bfc12e7c567790faf073b35953dd57a9a6db80
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-09-22 20:42:37 +0000

    edit

---
+++ while-loop.js
@@ -0,0 +1,21 @@
+// PH 18_5-3 More while loops, odd numbers, even numbers
+
+var num =1;
+// while(num <=8){
+//     console.log(num)
+//     num++;
+// }
+
+// odd number 
+
+while(num <=18){
+    console.log(num)
+    num += 2;
+}
+
+// even number 
+var number = 0;
+while(number<= 18){
+    console.log(number)
+    number += 2;
+}
