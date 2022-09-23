5c0a31a5f2f991f6adf309ccaba0d816f288a59d
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-09-23 14:47:05 +0000

    edit

--- pushpop.js
+++ pushpop.js
@@ -1,2 +1,9 @@
 // PH 18-4 Add or remove element from array using push, pop
 
+const friends =['mahtab','shawon','rubel', 'mona'];
+
+friends.push='pavel'
+
+friends.pop=[1];
+
+console.log(friends);
