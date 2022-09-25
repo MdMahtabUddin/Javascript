e4711cfa80788b29c94b3cc3d0d0852797d94071
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-09-25 14:59:29 +0000

    added

---
+++ switch.js
@@ -0,0 +1,15 @@
+// PH 19-7 (optional) Javascript switch case break and default
+
+// ব্যবহার করা হয় অনেকটা কন্ডিশনের মতই । 
+
+var color ='';
+
+if(color=='green'){
+    console.log('color is green')
+}else if (color=='bule'){
+    console.log('color is blue')
+}else if (color=='red'){
+    console.log('color is red')
+}else{
+    console.log('color is white')
+}
