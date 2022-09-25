db2707d2cebe5bc041c1e06f33066cf0e2e8c40e
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-09-25 15:13:25 +0000

    edit

--- switch.js
+++ switch.js
@@ -4,12 +4,35 @@
 
 var color ='';
 
-if(color=='green'){
-    console.log('color is green')
-}else if (color=='bule'){
+// if(color=='green'){
+//     console.log('color is green')
+// }else if (color=='bule'){
+//     console.log('color is blue')
+// }else if (color=='red'){
+//     console.log('color is red')
+// }else{
+//     console.log('color is white')
+// }
+
+
+// if/else প্রতিটি কন্ডিশন  চেক করে যায় ,switch কিন্তু সুইচ যেখানে মিল পাবে ঠিক সেই জায়গা তেই চলে যায় । 
+
+switch(color){
+    case'blue':
     console.log('color is blue')
-}else if (color=='red'){
-    console.log('color is red')
-}else{
-    console.log('color is white')
+    break;
+    case 'red':
+        console.log('color is red')
+        break;
+
+    case 'green':
+        console.log('color is green')
+        break;
+
+        default:
+            console.log('sorry color is black')
+
 }
+
+// switch faster then if/else 
+
