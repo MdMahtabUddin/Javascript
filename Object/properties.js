4e88daf17a5af2dc66154b5fc65a7c18eb3c8cae
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-09-30 17:14:45 +0000

    object  properties

---
+++ Object/properties.js
@@ -0,0 +1,13 @@
+// PH 36-3 Keys, values, entries, delete, seal, freeze
+
+const bottle={color:'red', hold:'water',price:45};
+
+// getting all properies name 
+const keys=Object.keys(bottle);
+// console.log(keys);
+
+// getting all properties value 
+const values = Object.values(bottle);
+console.log(values);
+
+
