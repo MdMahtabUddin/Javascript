1cb64f61971b9b79273f7ef063d9fc33208699e4
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-09-30 17:29:48 +0000

    object properties

--- Object/properties.js
+++ Object/properties.js
@@ -14,10 +14,17 @@ const values = Object.values(bottle);
 const entries= Object.entries(bottle);
 // console.log(entries)
 
+// property name never delete but change the value 
+Object.seal(bottle);
+bottle.price= 255;
+
+// similar with seal but never change anything 
+Object.freeze(bottle);
+
 // delete property 
 
 delete bottle.price;
-// console.log(bottle)
+console.log(bottle)
 
 
 
