d3bc6c9f69b48e7383fd4983d75f4f27c6eaad6d
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-10-01 15:57:47 +0000

    edit

--- Array/array.js
+++ Array/array.js
@@ -1,13 +1,13 @@
 // PH 18-3 Array index, get and set by index, indexOf
 
-// var students =['shamim', 'pavel','rubel', 'joy','ardina']
-// // check index number 
-// var classMates = students.indexOf('pavel');
+var students =['shamim', 'pavel','rubel', 'joy','ardina']
+// check index number 
+var classMates = students.indexOf('pavel');
 
-// // set a new name at index 
-// students[2]= 'shawon';
-// console.log(classMates);
-// console.log(students);
+// set a new name at index 
+students[2]= 'shawon';
+console.log(classMates);
+console.log(students);
 
 // kono element array te na thakle indexof dia khujle minus 1 dekhbe 
 
