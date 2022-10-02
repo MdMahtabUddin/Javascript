05fd34a942439cdaa18728a31cb63ec8f4bd7660
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-10-02 11:03:06 +0000

    chepest phone

---
+++ Problems/chepest-phone.js
@@ -0,0 +1,11 @@
+// PH 22-7 Find the cheapest phone from an array of phone objects
+
+const phone=[
+    
+    {name:'Samsung s7', price:30000, camera:16, memory:64},
+    {name:'Oneplus 10', price:80000, camera:32, memory:128},
+    {name:'I-Phone14', price:1500000, camera:32, memory:264},
+    {name:'Nokia n95', price:25000, camera:16, memory:64},
+    {name:'Xiomi m30', price:15000, camera:8, memory:128}
+];
+
