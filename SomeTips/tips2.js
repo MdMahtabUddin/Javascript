e86b8e8d4f67341af6479b08577abab9b833048b
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-10-05 22:56:47 +0600

    using temp

--- SomeTips/tips2.js
+++ SomeTips/tips2.js
@@ -1,6 +1,12 @@
-// LWS Tips2
+// LWS Tips2=11
 // 1. swaping values  
+const array =[24,45,65,85,35];
+// using temp  
+let temp =array[0];
+array[0]=array[4];
+array[4]=temp;
 
+// console.log(array);
 
 
 
