b8b2a6d07a69a105c36921bfe05fd4a4133d9035
MdMahtabUddin <mahtab.sentu@outlook.com>
2022-10-09 02:33:34 +0600

    default parametter

--- ES6/defaultFunction.js
+++ ES6/defaultFunction.js
@@ -1,10 +1,16 @@
 // 31-4 Function default parameter for not provided values
+// কোন কারণে যদি প্যারামিটার দেয়া না হয় । তখন ডিফল্ট প্যারামিটার ব্যবহার করা হয় second =0 । 
 
-function num(first, second){
+function num(first, second =0){
+
+    // before ES6 this method apply 
+    // if (second === undefined){
+    //     second = 0;
+    // }
     const total = first + second;
 
     return total
 }
 
-const myNumber =num(1, 11);
+const myNumber =num(11);
 console.log(myNumber);
