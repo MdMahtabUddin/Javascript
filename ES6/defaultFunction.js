d5d7c25091a416495ebff80fae5797c2e7cf1e0d
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-10-08 21:24:24 +0600

    added

---
+++ ES6/defaultFunction.js
@@ -0,0 +1,10 @@
+// 31-4 Function default parameter for not provided values
+
+function num(first, second){
+    const total = first + second;
+
+    return total
+}
+
+const myNumber =num(1, 11);
+console.log(myNumber);
