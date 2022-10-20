5bb617fe5df2e04158a3611e4d18892f447ed832
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-10-20 10:58:51 +0000

    added

---
+++ string-slice.js
@@ -0,0 +1,11 @@
+// 29-3 How to split, slice, substr, substring, concat, join
+
+const anthem ='amar sonar bangla , ami tomay valobasi';
+
+const words =anthem.split(' ')
+// console.log(words)
+const withoutA = anthem.split('a');
+// console.log(withoutA)
+
+const anthem2 =anthem.slice(5,11)
+console.log(anthem2)
