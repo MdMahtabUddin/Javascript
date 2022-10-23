01c5b67179c9d5808bb96a7272e599ee6c50a4a1
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-10-23 06:50:23 +0000

    array slice & Splice

--- array-slice.js
+++ array-slice.js
@@ -1,2 +1,30 @@
 // 29-5 Get part of an array and insert elements using slice, splice
 
+const number =[50,33,45,27,12,99,30];
+
+
+const numberslice =number.slice(3,5)
+
+
+// remove from array  
+const numberSplice =number.splice(3,2)
+
+
+// added number in array  
+const numberSplice2 =number.splice(3,2,70,999,333)
+// console.log(numberSplice2)
+console.log(number)
+
+
+// যদি শুরু অনিধারিত হয় তবে ইনডেক্স জিরো থেকে স্লাইস শুরু হয় । 
+// স্লাইস করলেও আসল এঁরে পরিবর্তন হবে না । 
+
+// স্প্লাইস দিয়ে এঁরে থেকে রিমুভ করা হয় । 
+// স্প্লাইস আসল এরে কে পরিবর্তন করে । রিমুভ করা ইনডেক্স ভ্যালু থাকবে না । 
+
+// স্প্লাইস দিয়ে নতুন মান ও যোগ করা যাবে । 
+
+
+
+
+
