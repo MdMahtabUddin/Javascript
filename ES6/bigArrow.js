0b32d293270b2ee4cd80860feb1ac484522e15f8
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-10-24 09:55:40 +0000

    arrow function

--- ES6/bigArrow.js
+++ ES6/bigArrow.js
@@ -1,7 +1,32 @@
 // 31-7 More Arrow functions and big arrow function
 // using default parametter num2=25
 
-const number =(num1, num2=25)=>num1+num2;
+// arrow function এ প্যারামিটার একের অধিক হলে ব্রাকেট ব্যবহার করতে হবে , যদি না থাকে তাহলেও করতে হবে। 
+// যদি একটা প্যারামিটার থাকে ব্রাকেট ব্যবহার না করলেও চলবে । 
 
-const newNumber =number(35)
-console.log(newNumber);
+// const number =(num1, num2=25)=>num1+num2;
+
+// const newNumber =number(35)
+// console.log(newNumber);
+
+
+
+// multiline arrow function  
+
+const doMath =(x,y)=>{
+    const sum = x+y;
+    const diff =x-y;
+    const result = diff * sum;
+    const output =result *5;
+    return output;
+
+}
+const myNumber =doMath(50,23)
+console.log(myNumber)
+
+// মাল্টিলাইন করতে গেলে সেকেন্ড ব্রাকেট ব্যবহার করতে হবে । 
+
+// সিঙ্গেল লাইনে রিটান করতে হবে না , তবে মাল্টিলাইনের ক্ষেত্রে রিটান করতে হবে । 
+
+// সিঙ্গেল লাইনে অটো রিটান করে তাই ইমপ্লিসিট রিটান বলে । 
+// মাল্টিলাইনে যেহেতু রিটান লিখে দিতে হয় , তাই এক্সপ্লিসিট রিটান বলে । 
