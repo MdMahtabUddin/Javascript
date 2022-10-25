9d66147083d585929b2a914fbdb8298a5b9a28b9
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-10-25 06:30:01 +0000

    added

---
+++ ES6/chaining.js
@@ -0,0 +1,16 @@
+// 32-3 (advanced) Array Destructuring, nested object Optional chaining
+
+// declare varriable based on the object property 
+const myVarriable ={x:20,y:50,z:70}
+
+const {x,y }=myVarriable
+
+// console.log(x ,y)
+
+// destructuring array 
+const [shawon, mona, pavel ] =[20,50,70]
+
+// console.log(shawon)
+
+// chaining 
+?? 
