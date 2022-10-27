5ec859cceb877c97b7620557ea86dacd67dea128
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-10-27 07:36:20 +0000

    Sets

---
+++ set.js
@@ -0,0 +1,16 @@
+// LWS 41
+
+// javascript is a collection of unique value 
+
+// create a set 
+
+const litters = new Set([23,45,70]);
+
+// add a value 
+litters.add(70);
+litters.add(99)
+
+// সেট এ একই রকম  ভ্যালু অ্যাড করা যাবে না 
+console.log(litters)
+
+
