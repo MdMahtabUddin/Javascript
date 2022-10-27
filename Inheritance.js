04b436762ea023918bf3308837cc339204256196
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-10-27 10:20:35 +0000

    edit

---
+++ Inheritance.js
@@ -0,0 +1,25 @@
+
+
+
+class Support{
+    name;
+    designation='support instructor'
+    address='BD'
+    constructor(name){
+        this.name=name;
+    
+    }
+    supportSession(){
+        console.log('start a session')
+    }
+    }
+    
+    const mahtab =new Support('Md Mahtab')
+    const sawon =new Support('jahir uddin')
+    
+    
+    mahtab.supportSession()
+    // console.log(mahtab)
+    // console.log(sawon)
+
+    
