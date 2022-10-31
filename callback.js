d14ce7d1a253ba91618c72557e7c155b76fbab05
MdMahtabUddin <mahtab.sentu@outlook.com>
2022-10-31 23:00:23 +0600

    callback function

--- callback.js
+++ callback.js
@@ -1,2 +1,13 @@
 
 // 35-8 (advanced) Callback function and pass different functions
+
+// একটা fangshon কে যদি আরেকটা ফাংশনের প্যারামিটার হিসাবে পাঠানো হয় সেটাকে callback ফাংশন বলে । 
+
+function welcome (name, greetHandler){
+greetHandler(name)
+}
+function greetMorning(name){
+    console.log('Good Morning', name)
+
+}
+welcome('mahtab',greetMorning)
