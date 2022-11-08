6b1d50a83275d8156061eeb2fb902f47733b084f
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-11-08 19:35:55 +0000

    math

---
+++ Math/math.js
@@ -0,0 +1,18 @@
+// PH-21.1
+
+const myNumber = -5;
+// const output = Math.abs(myNumber);
+
+// Math.abs always return positive value 
+
+const myNumber2 =7.103;
+// const output = Math.ceil(myNumber2);
+// Math.celi always return nearest top number 
+
+// const myNumber3 =5.999;
+// const output =Math.floor(myNumber3);
+// Math.floor always return nearest bottom number 
+
+// const myNumber4 =Math.random() * 6;
+// const rounded = Math.round(myNumber4);
+console.log(output);
