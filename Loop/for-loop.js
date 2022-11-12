9d162a9d2606b78d7fe10d2551efb16a6406345e
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-11-12 17:25:11 +0000

    edit

--- Loop/for-loop.js
+++ Loop/for-loop.js
@@ -2,6 +2,7 @@ var numbers =[27,33,82,45,12,55];
 
 // লুপ এঁরের ইনডেক্স নম্বার এর উপর চালানো হয় । পরবর্তী ইনডেক্স বাড়লেও যেন কাউন্ট করা যায় 
 //  এঁর জন্য indexedDB.length ব্যবহার করা হয় । 
+
 for (var i =0; i< numbers.length; i++){
     var element = numbers[i]
     console.log(element)
