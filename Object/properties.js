ca20dddde5183ed0378f4b49b0f63cfcee1365a4
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-11-16 16:31:18 +0000

    review

--- Object/properties.js
+++ Object/properties.js
@@ -1,30 +1,35 @@
 // PH 36-3 Keys, values, entries, delete, seal, freeze
 
-const bottle={color:'red', hold:'water',price:45};
+// const bottle={color:'red', hold:'water',price:45};
+const products={color:'red', hold:'water',price:45};
+
 
 // সব প্রোপার্টির নাম একসাথে পেতে চাইলে keys ব্যবহার করা হয় । 
-const keys=Object.keys(bottle);
+// const keys=Object.keys(bottle);
+// const keys =Object.keys(products)
 
 // সব প্রোপার্টির ভ্যালু একসাথে পেতে চাইলে values ব্যবহার করা হয় । 
-const values = Object.values(bottle);
+// const values = Object.values(bottle);
+const values =Object.values(products)
 
 // সব প্রোপার্টির নাম এবং ভ্যালু একসাথে এরের ভিতর পেতে চাইলে Entries ব্যবহার করা হয় । 
-const entries= Object.entries(bottle);
+// const entries= Object.entries(bottle);
 
 
 // property name never delete but change the value 
-Object.seal(bottle);
-bottle.price= 255;
+// Object.seal(bottle);
+// bottle.price= 255;
 
 // similar with seal but never change anything 
-Object.freeze(bottle);
+// Object.freeze(bottle);
 
 // কোন প্রপার্টি ডিলিট করতে চাইলে ব্যবহার করা হয় 
 // কিন্তু আগে যদি seal বা freese ব্যবহার করা হয় তাহলে ডিলিট করা যাবে না । 
 // seal মডিফাই করা যাবে , কিন্তু freeze ব্যবহার করলে কোন কিছুই পরিবর্তন করা যাবে না । 
 
-delete bottle.price;
-console.log(bottle)
+// delete bottle.price;
+// console.log(bottle)
+console.log(products)
 
 
 
