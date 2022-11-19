0e9ef9c5ac4eb76d33983a0064c6dcda7128b0a8
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-11-19 16:23:02 +0000

    revision

--- Problems/array-sum.js
+++ Problems/array-sum.js
@@ -2,11 +2,18 @@
 
 // এরের সব এলিমেন্ট এর যোগ ফল বের করার জন্য । 
 
-const numbers =[32,45,12,50,70];
+const numbers =[32,45,12,50,70,55,15];
 let sum =0; 
 
-for(let i =0;i< numbers.length; i++){
-    const elements =numbers[i]
-    sum += elements;
+// for(let i =0;i< numbers.length; i++){
+//     const elements =numbers[i]
+//     sum += elements;
+// }
+// console.log(sum);
+
+// revision
+// 19nov
+for(let number of numbers){
+    sum += number
 }
-console.log(sum);
+console.log(sum)
